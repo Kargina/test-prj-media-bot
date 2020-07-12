221e069dd259f1e64796025c2fd99c544b844aa8
Ksenia Kargina <kargina.ks@gmail.com>
2020-07-13 00:53:44 +0300

    refactoring, add some exceptions, reply instead of answer, rename README.md

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM kargina/python-dlib:latest
 RUN apt-get update && apt-get install -y ffmpeg
-COPY bot.py /app/bot.py
-COPY requirements.txt /app/requirements.txt
-RUN pip install -r /app/requirements.txt
-ENTRYPOINT python /app/bot.py
+COPY . /app/
+WORKDIR /app
+RUN pip install -r requirements.txt
+ENTRYPOINT python bot.py
--- bot.py
+++ bot.py
@@ -7,64 +7,105 @@ import shutil
 import ffmpeg
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+SAMPLE_RATE = 16000
+IMAGE_SCALE = 1
 
 logging.basicConfig(level=logging.INFO)
 
 api_key = os.getenv("TELEGRAM_TOKEN", "")
+
 if not api_key:
-    logging.info("Please set env var TELEGRAM_TOKEN")
+    logging.info("Env var TELEGRAM_TOKEN must be set, exit...")
     exit(1)
 
 bot = Bot(token=api_key)
 dp = Dispatcher(bot)
 
 
+class MediaBotError(Exception):
+    pass
+
+
+def create_user_dir(dir_name, user_id):
+    destination_dir = os.path.join(FILE_PATH, "data", dir_name, str(user_id))
+    if not os.path.exists(destination_dir):
+        try:
+            os.makedirs(destination_dir)
+        except OSError as e:
+            logging.error(f" {destination_dir!r}: {e}")
+            raise MediaBotError
+
+    return destination_dir
+
+
+@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends start/help or any message
+    """
+    await message.answer("Hi!\nSend me photo or voice message.")
+
+
 @dp.message_handler(content_types=types.ContentTypes.VOICE)
 async def process_voice(message: types.Message):
-    """Convert voice messages to wav format with sample rate 16kHz"""
-    destination_dir = os.path.join(FILE_PATH, "data", "audio", str(message.from_user.id))
-
+    """
+    Convert voice messages to wav format with sample rate 16kHz
+    """
     with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_file = os.path.join(tmp_dir, f'{message.from_user.id}.oga')
-        await message.voice.download(destination=tmp_file)
+        tmp_file_path = os.path.join(tmp_dir, f'{message.from_user.id}.oga')
+        await message.voice.download(destination=tmp_file_path)
 
-        if not os.path.exists(destination_dir):
-            os.makedirs(destination_dir)
+        try:
+            destination_dir = create_user_dir('audio', message.from_user.id)
+        except MediaBotError:
+            await message.reply("Can't save file, sorry")
+            return
+        file_num = len(os.listdir(destination_dir))
+        destination_path = os.path.join(destination_dir, f'audio_message_{file_num}.wav')
 
-        next_file_num = len(os.listdir(destination_dir))
-        destination_path = os.path.join(destination_dir, f'audio_message_{next_file_num}.wav')
+        stream = ffmpeg.input(tmp_file_path)
+        stream = ffmpeg.output(stream, destination_path, ar=SAMPLE_RATE)
+        try:
+            ffmpeg.run(stream)
+        except Exception as e:
+            msg = "Can't decode audio file"
+            logging.error(f"{msg} {tmp_file_path!r}: {e}")
+            await message.reply(msg)
 
-        stream = ffmpeg.input(tmp_file)
-        stream = ffmpeg.output(stream, destination_path, ar='16000')
-        ffmpeg.run(stream)
-
-    with open(destination_path, 'rb') as audio:
-        await message.answer_document \
-            (audio, caption='Audio was saved.\n'
-                            'This is your file converted to wav with sample rate 16kHz')
+    try:
+        with open(destination_path, 'rb') as audio:
+            await message.reply_document(audio, caption='Audio was saved.\n'
+                                                        'This is your file converted '
+                                                        'to wav with sample rate 16kHz.')
+    except IOError as e:
+        logging.error(f"Can't write to file {destination_path!r}: {e}")
+        await message.reply("File was decoded, but i can't save it, sorry.")
 
 
 @dp.message_handler(content_types=types.ContentTypes.PHOTO)
 async def process_photo(message: types.Message):
-    """Search faces within a photo. Save photo if face presence."""
+    """
+    Search faces within a photo. Save photo if face presence
+    """
     with tempfile.TemporaryDirectory() as tmp_dir:
-
-        tmp_path = os.path.join(tmp_dir, f'{message.photo[-1].file_id}.jpg')
-        await message.photo[-1].download(destination=tmp_path)
+        tmp_file_path = os.path.join(tmp_dir, f'{message.photo[-1].file_id}.jpg')
+        await message.photo[-1].download(destination=tmp_file_path)
 
         detector = dlib.get_frontal_face_detector()
-        img = dlib.load_rgb_image(tmp_path)
-        face_list = list(detector(img, 1))
+        img = dlib.load_rgb_image(tmp_file_path)
+        face_list = list(detector(img, IMAGE_SCALE))
 
         if face_list:
-            destination_path = os.path.join(FILE_PATH, "data", "photos")
-            if not os.path.exists(destination_path):
-                os.makedirs(destination_path)
-            shutil.move(tmp_path, destination_path)
-            await message.answer(f"Detected faces: {len(face_list)}.\nPhoto saved.")
-
+            try:
+                destination_dir = create_user_dir("photos", message.from_user.id)
+            except MediaBotError:
+                await message.reply("Can't save file, sorry")
+                return
+            shutil.move(tmp_file_path, destination_dir)
+            await message.reply(f"Detected faces: {len(face_list)}.\nPhoto saved.")
         else:
-            await message.answer(f"No faces found")
+            await message.reply(f"No faces found")
 
 
 if __name__ == '__main__':
